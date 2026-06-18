ad521f4e6ad1ad7fe5ac9a0b6e6374678f0c80e5
otto999999999 <lenwweith99@gmail.com>
2026-06-18 15:51:24 +0200

    Fine tune mobile date time input width

--- app/logging/page.tsx
+++ app/logging/page.tsx
@@ -830,7 +830,7 @@ const groupedDoses = filteredDoses.reduce<Record<string, Dose[]>>((acc, dose) =>
     onChange={(e) => setForm({ ...form, datum: e.target.value })}
     className={inputClass}
     style={{
-      width: "88%",
+      width: "90%",
       marginLeft: "auto",
       marginRight: "auto",
       display: "block",
@@ -845,7 +845,7 @@ const groupedDoses = filteredDoses.reduce<Record<string, Dose[]>>((acc, dose) =>
     onChange={(e) => setForm({ ...form, uhrzeit: e.target.value })}
     className={inputClass}
     style={{
-      width: "88%",
+      width: "90%",
       marginLeft: "auto",
       marginRight: "auto",
       display: "block",
